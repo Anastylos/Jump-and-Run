90999c6293c3bad81291cb3a32e7d85db13b838a
JustMischka <michael.guenter1@web.de>
2024-10-24 13:00:46 +0200

    Enemy hit animation + Bow shoot bug
    Enemy hit animation + Bow shoot bug

--- Godot Game/scenes/nahkampf_gegner.gd
+++ Godot Game/scenes/nahkampf_gegner.gd
@@ -5,16 +5,26 @@ var hp = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
+	if not is_on_floor():
+			velocity += get_gravity() * delta
+	
 	if hp <= 0:
 		queue_free()
+		
+	move_and_slide()
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("projectile"):
 		animated_sprite_2d.play("hit")
 		hp -= 50
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite_2d.animation == "hit":
+		animated_sprite_2d.play("Idle")
