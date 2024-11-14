c6716685d116c57bfe854de53e63c8a37a697169
Anastylos <157839647+Anastylos@users.noreply.github.com>
2024-11-14 10:50:00 +0100

    totem animation and collisionbox
    updated totem collisionbox: the player does not exit the box while jumping
    totems play an animation while the player is in its area

--- Godot Game/scenes/fire_totem.gd
+++ Godot Game/scenes/fire_totem.gd
@@ -1,11 +1,17 @@
 extends Area2D
 
+@onready var totem_fire_animation = $fire_totem_AnimatedSprite2D
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		body.set_totemStatus("fire")
+		totem_fire_animation.visible = true
+		totem_fire_animation.play("fire")
 
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		body.set_totemStatus("none")
+		totem_fire_animation.visible = false
+		
+		
